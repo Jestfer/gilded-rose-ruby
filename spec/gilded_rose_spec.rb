56e1a416c402d6cffdb1fe342d1c40f6e0dd4387
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-27 19:37:34 +0100

    testing quality is degraded twice as fast when item is expired

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -14,5 +14,13 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 9
     end
+
+    it "degrades avg item qty by 2 when item expired" do
+      items = [Item.new("chicken", 0, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 8
+    end
+
+    
   end
 end
