275d97f327c5afbe584011d28512d598a4d36050
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-29 10:34:18 +0100

    refactoring with classes and self-update method: deadline_item

--- lib/deadline_item.rb
+++ lib/deadline_item.rb
@@ -6,4 +6,25 @@ class DeadlineItem < CommonItem
     @sell_in = sell_in
     @quality = quality
   end
+
+  def update_item
+    next_day
+    increase_qty
+    check_expiracy
+  end
+end
+
+private
+
+def check_expiracy
+  bin_passes if @sell_in < 0
+end
+
+def bin_passes
+  @quality = MIN_QTY
+end
+
+def increase_qty
+  return @quality += 3 if @sell_in < 5
+  @quality += 2 if @sell_in < 10
 end
